4c28cafccd6e1ccc96ea87f941165da9b8667d2e
Brunoficial <bruno.lopes.vrls@gmail.com>
2026-01-22 19:29:15 -0300

    feat: Criado um novo model para Transações

---
+++ backend/src/models/Transaction.py
@@ -0,0 +1,34 @@
+from backend.src.models.enums.AccountTypes import AccountTypes
+from backend.src.models.enums.TransactionCategories import TransactionCategories
+from backend.src.models.enums.TransactionTypes import TransactionTypes
+from ..config.db import db 
+
+class Transaction (db.Model): 
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    description = db.Column(db.String(1000), nullable=True)
+    date = db.Column(db.Date)
+    value = db.Column(db.Float, nullable=False)
+    type = db.Column(db.Enum(TransactionTypes, name="transaction_type"), nullable=False)
+    category = db.Column(db.Enum(TransactionCategories, name="transaction_category"), nullable=False)
+    account_type = db.Column(db.Enum(AccountTypes, name="account_type"), nullable=False)
+
+    def __init__(self, description, date, value, type, category, account_type):
+        self.description = description
+        self.date = date
+        self.value = value
+        self.type = type
+        self.category = category
+        self.account_type = account_type
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "date": self.date,
+            "value": self.value,
+            "type": self.type,
+            "category": self.category,
+            "account_type": self.account_type
+        }
